c46cf14f4c648e6a9b6de8bdb0ea4f4d559ad87e
Sanjula <sanjula8@gmail.com>
2020-02-01 12:00:17 -0600

    timer working, beginning local to save to localstorage but unable to pull out of localstorage yet

--- Develop/script.js
+++ Develop/script.js
@@ -2,31 +2,52 @@
 
 var $currentDay = $("#currentDay");
 var $saveBtn = $(".saveBtn");
+var $hour9 = $("#hour-9");
+var $hour10 = $("#hour-10");
+var $hour11 = $("#hour-11");
+var $hour12 = $("#hour-12");
+var $hour13 = $("#hour-13");
+var $hour14 = $("#hour-14");
+var $hour15 = $("#hour-15");
+var $hour16 = $("#hour-16");
+var $hour17 = $("#hour-17");
 
 // Set Dates
 
 // Current Time
-
 var currentHour = moment().hours();
 function clock() {
-	var now = moment().format("MMMM Do YYYY, h:mm:ss a");
+	var now = moment().format("MMMM Do YYYY, h:mm:ss A");
 	$currentDay.html(now);
-	console.log(now);
+	setTimeout(clock, 1000);
+	// console.log(now);
 }
 
 clock();
 
-// $(document).ready(function() {
-//     var interval = setInterval(function() {
-//         var momentNow = moment();
-//         $('#date-part').html(momentNow.format('YYYY MMMM DD') + ' '
-//                             + momentNow.format('dddd')
-//                              .substring(0,3).toUpperCase());
-//         $('#time-part').html(momentNow.format('A hh:mm:ss'));
-//     }, 100);
+// Save To Local Storage
 
-//     $('#stop-interval').on('click', function() {
-//         clearInterval(interval);
+// var saveData = JSON.parse(localStorage.saveData || null || {});
+
+// Store Data
+$(".saveBtn").on("click", function(e) {
+	e.preventDefault();
+	localStorage.setItem("input", JSON.stringify(input));
+	var input = $hour9.val();
+	console.log(input);
+});
+// lastSave();
+
+// function lastSave() {
+// 	var savedThing = JSON.parse(localStorage.getItem("input"));
+// 	$hour9.text(savedThing);
+// }
+
+// Clear All
+// $(".clearBtn").on("click", function() {
+
+//     )}
+
 // Set Time Increments
 
 // Make past events grayed out
